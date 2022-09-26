ef737ec53a5b503c333bab9a9761a3958e0597a1
WallKing110 <57759414+WallKing110@users.noreply.github.com>
2022-09-26 10:03:41 +0500

    Add files via upload

--- rle/rle.js
+++ rle/rle.js
@@ -1,14 +1,9 @@
 const fs = require("fs")
 var arg = process.argv;
 
-var mode = arg[2];
-var fileInput = arg[3];
-var fileOutput = arg[4]; 
-
-let inText = fs.readFileSync(fileInput, "utf-8");
-let outText = "";
+let inText = fs.readFileSync(arg[3], "utf-8");
 //КОДИРОВКА
-if (mode == "encode")
+if (arg[2] == "encode")
 {
 	for (var str_point=0, repeat_count = 1, str_result = ''; str_point <= inText.length; ++str_point) {
 		if (inText.charAt(str_point) == inText.charAt(str_point+1)) repeat_count++;
@@ -25,32 +20,31 @@ if (mode == "encode")
 			repeat_count = 1;
 		}
 	}
-	fs.writeFile(fileOutput, str_result);
+	fs.writeFileSync(arg[4], str_result);
 	
-	let fileInputSize = fs.statSync(fileInput).size;
-	let fileOutputSize = fs.statSync(fileOutput).size;
-	console.log(`Размер изначального файла: ${fileInputSize}`)
-	console.log(`Размер файла на выходе: ${fileOutputSize}`)
-	console.log(`Коэффициент сжатия: ${fileInputSize/fileOutputSize}`)
+	let fileInSize = fs.statSync(arg[3]).size;
+	let fileOutSize = fs.statSync(arg[4]).size;
+	console.log(`Размер изначального файла: ${fileInSize}`)
+	console.log(`Размер файла на выходе: ${fileOutSize}`)
+	console.log(`Коэффициент сжатия: ${fileInSize/fileOutSize}`)
 }
 else//ДеКодировка
 {
 	let counter = 0;
-	let str_result = '';
-	while (true){
-		if (counter == inText.length) break;
+	let outText = "";
+	while (counter != inText.length){
 		if (inText.charAt(counter) == "#"){
 			let str_temp = "";
 			for (let k = 0; k < inText.charCodeAt(counter+1); k++){
 				str_temp += inText.charAt(counter+2);
 			}
-			str_result += str_temp;
+			outText += str_temp;
 			counter+=3;
 		}
 		else{
-			str_result += inText.charAt(counter);
+			outText += inText.charAt(counter);
 			counter+=1;
 		}
 	}
-	fs.writeFileSync(fileOutput, str_result);
+	fs.writeFileSync(arg[4], outText);
 }
