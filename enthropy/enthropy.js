376866b13479cadb4656ca41469ed953a662422b
WallKing110 <57759414+WallKing110@users.noreply.github.com>
2022-10-09 20:27:20 +0500

    Add files via upload

--- enthropy/enthropy.js
+++ enthropy/enthropy.js
@@ -1,6 +1,5 @@
-//ФИО - Саламахин Кирилл П.
 const fs = require("fs");
-var arg = process.argv;
+let arg = process.argv;
 
 function getBaseLog(x, y) {
   return Math.log(y) / Math.log(x);
@@ -8,10 +7,10 @@ function getBaseLog(x, y) {
 
 let file = fs.readFileSync(arg[2], "utf-8");
 
-if (file.length == 1){
+/*if (file.length == 1){
 	console.log('1');
 	return 1
-}
+}*/
 var alphSize = 0;
 var alphQuant = 0;
 alph = new Array();//Инициализация массива
@@ -26,10 +25,15 @@ for (let i = 0; i<file.length;i++){
 	alphQuant+=1;
 }
 
-var enthropy = 0;
+let enthropy = 0;
 for (i in alph){
 	alph[i] /= alphQuant;//Вероятность вместо элементов
+	console.log(i, '|', alph[i]);
+	if (alphSize == 0){
+		enthropy = 0;
+		break;
+	}
 	enthropy -= alph[i]* getBaseLog(alphSize,alph[i])//Подсчет элемента массива как элемента суммы энтропии+сложение
 }
 
-console.log(enthropy);
+console.log("Энтропия равна ", enthropy);
